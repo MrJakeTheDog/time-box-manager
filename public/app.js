fbc3e81f10242af06b91c88a8f519c428d62bccb
Mikhail <ekazamen_na_5@ukr.net>
2020-03-12 23:46:25 +0200

    add login

--- public/app.js
+++ public/app.js
@@ -3,14 +3,81 @@ document.addEventListener("DOMContentLoader", event => {
     console.log(app);
 })
 
+function signUp(){
+        
+    const userEmail = document.getElementById("email_field").value;
+    const userPass = document.getElementById("password_field").value;
+    
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+    .then(function(){
+        window.alert("SignUp!!");
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    })
+}
+
+
+function login(){
+  
+    const userEmail = document.getElementById("email_field").value;
+    const userPass = document.getElementById("password_field").value;
+  
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then(function(){
+        document.location.href = "main.html";
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+}
+  
+
 function googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+    var provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider)
         .then(result => {
-            const user = result.user;
-            document.write('Hello '+user.displayName);
+            var user = result.user;
+            //document.write('Hello '+user.displayName);
             console.log(user)
+            document.location.href = "main.html";
         })
         .catch(console.log)
 }
+
+function facebookLogin() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            var token = result.credential.accessToken;
+            var user = result.user;
+            //document.write('Hello '+user.displayName);
+            console.log(user)
+            document.location.href = "main.html";
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+}
+  
